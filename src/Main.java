182dea3b05006fafe7b691c309be021e462452b6
Alexander <alexander.ostman@hotmail.com>
2015-10-07 16:56:05 +0200

    La till grunder till att göra en prediction

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Main {
 
@@ -10,10 +13,28 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		Path p = FileSystems.getDefault().getPath("big_test.arpa");
-		ArpaRead reader = new ArpaRead(p);
+		
+		NGrams grams = new NGrams(p);
+		
+		grams.GetPrediction("Hello");
+		/*ArpaRead reader = new ArpaRead(p);
 		
 		HashMap<Integer, NGramHandler> grams = reader.GetNGrams();
 		
+		
+		Iterator<Entry<Integer, NGramHandler>> it = grams.entrySet().iterator();
+		
+		while(it.hasNext())
+		{
+			Entry<Integer, NGramHandler> entry = it.next();
+			
+			NGramHandler handler = entry.getValue();
+			if(handler != null)
+			{
+				handler.Sort();
+			}
+		}*/
+		//grams.get(0).
 		//grams.get(2).
 		//System.err.println("hej");
 	}
--- src/NGram.java
+++ src/NGram.java
@@ -1,4 +1,4 @@
-public class NGram {
+public class NGram implements Comparable<NGram> {
 
 	private String[] Words;
 	private double Probability;
@@ -35,5 +35,19 @@ public class NGram {
 	public double GetBackoff()
 	{
 		return Backoff;
+	}
+
+	@Override
+	public int compareTo(NGram o) {
+		
+		int compareValue = 0;
+		for(int i = 0; i < Words.length; i++)
+		{
+			compareValue = Words[i].compareTo(o.GetWord(i));
+			if(compareValue != 0)
+				return compareValue;
+		}
+		// TODO Auto-generated method stub
+		return compareValue;
 	}
 }
