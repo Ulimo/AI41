72d17922f7dca47cdb70586ae3fba249e942e9ad
Alexander <alexander.ostman@hotmail.com>
2015-10-12 12:28:16 +0200

    Verbs in succession grammar check

--- src/Grammar.java
+++ src/Grammar.java
@@ -1,12 +1,22 @@
 
 public class Grammar {
 	
-	public boolean getGrammar(String[] sentence,String [] tags, double[] probs){
+	public static boolean getGrammar(String[] sentence,String [] tags, double[] probs){
 		
+	boolean returnValue = true;	
 		
-		
-		
+	returnValue = returnValue & verbsInSuccession(sentence, tags, probs);
 	
- return true;
+	
+ return returnValue;
 }
+	
+	
+	private static boolean verbsInSuccession(String[] sentance, String[] tags, double[] probs)
+	{
+		if(tags[tags.length -2].charAt(0) == 'V' && tags[tags.length - 1].charAt(0) == 'V')
+			return false;
+		
+		return true;
+	}
 }
