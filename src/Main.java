650326b604b898c5ae9d5a03fd122e45ce29880a
Mikael <miksj@kth.se>
2015-10-12 18:19:52 +0200

    Wildcard i main

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,6 @@ import opennlp.uima.postag.*;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 	
 		Path p = FileSystems.getDefault().getPath("model.arpa");
 		System.out.println("Creating datastructure for N-grams...");
@@ -30,12 +29,18 @@ public class Main {
 		
 		while(true){
 			in = new Scanner(System.in);
+			System.out.print("Number of predictions [1,2,...]: ");
+			int numOfPred = in.nextInt();
+			System.out.print("Maximum number of N-grams [1,2,...]: ");
+			int maxNgSize = in.nextInt();
+			// Line below fixes something weird with nextLine(), #java quick fix...
+			in = new Scanner(System.in);
 			System.out.print("Write your sentence: ");
 			String sentence = in.nextLine();
 			
 			while(true){
 				System.out.println("\nGetting prediction for sentence: "+sentence);
-				NGram[] result = grams.GetPrediction(sentence, 10, 5);
+				NGram[] result = grams.GetPrediction(sentence, numOfPred, maxNgSize);
 				
 				for(int i=0; i<result.length; i++){
 					String currentWord = result[i].GetLastWord();
@@ -46,9 +51,19 @@ public class Main {
 				
 				if(input == -1 || input < -3  || input > result.length-1){
 					//Enter a new word and add it
-					System.out.println("Write the correct word: ");
-					String word = in.next();
-					sentence += (" " + word);
+					if(sentence.endsWith("*")){
+						sentence = sentence.substring(0, sentence.length()-1);
+						System.out.print(sentence);
+						String word = in.next();
+						sentence += (word);
+					}
+					else{
+						sentence += " ";
+						System.out.print(sentence);
+						String word = in.next();
+						sentence += (word);
+					}
+					
 				}
 				else if(input == -2){
 					//Restart
